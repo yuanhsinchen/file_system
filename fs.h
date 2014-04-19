1e16bdfec80d4bd313dd72306fd90a7f820a30d4
Yuan-Hsin Chen <yuanlmm@gmail.com>
2014-04-19 17:49:18 -0400

    modified type name of descriptors

--- fs.h
+++ fs.h
@@ -6,7 +6,7 @@ typedef struct file_descriptor {
 char fname[64]; /* filename */
 int fsize; /* file size */
 uint16_t bid[30]; /* block id of the file */
-}fd;
+}file_desc;
 
 struct entry {
 uint16_t bid;
@@ -17,10 +17,10 @@ typedef struct dir_descriptor {
 char dname[64]; /* directory name */
 int dnum; /* how many files and directories in it? */
 struct entry e[15]; /* entry block of the files and directories */
-}dd;
+}dir_desc;
     
 typedef struct superblock {
 int root_bid; /* block id of root directory */
 int fs_size; /* size of the file system */
 uint32_t bitmap; /* bitmap of whether a block is occupied */
-}sb;
+}superblock;
