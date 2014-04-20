a9e11c569f7d42b46907fe9029a538b8077b1ab6
Yuan-Hsin Chen <yuanlmm@gmail.com>
2014-04-19 23:45:02 -0400

    indent

--- fs.h
+++ fs.h
@@ -3,30 +3,30 @@
 /* Each descriptor is 1024 Byte which is the same as block size */
 
 typedef struct file_descriptor {
-char fname[255]; /* filename */
-int fsize; /* file size */
-uint16_t bid[382]; /* block id of the file */
-}file_desc;
+  char fname[255]; /* filename */
+  int fsize; /* file size */
+  uint16_t bid[382]; /* block id of the file */
+} file_desc;
 
 struct entry {
-uint16_t bid;
-uint8_t type; /* type of entry: file=1 or directory=0 */
+  uint16_t bid;
+  uint8_t type; /* type of entry: file=1 or directory=0 */
 };
 
 typedef struct dir_descriptor {
-char dname[255]; /* directory name */
-int dnum; /* how many files and directories in it? */
-struct entry e[191]; /* entry block of the files and directories */
-}dir_desc;
+  char dname[255]; /* directory name */
+  int dnum; /* how many files and directories in it? */
+  struct entry e[191]; /* entry block of the files and directories */
+} dir_desc;
     
 typedef struct superblock {
-int root_bid; /* block id of root directory */
-int fs_size; /* size of the file system */
-/* 40 * 1024 blocks means 40 * 1024 bits for bitmap */
-/* 5 blocks needed */
-/* bitmap bids are 1 - 5 */
-}superblock;
+  int root_bid; /* block id of root directory */
+  int fs_size; /* size of the file system */
+  /* 40 * 1024 blocks means 40 * 1024 bits for bitmap */
+  /* 5 blocks needed */
+  /* bitmap bids are 1 - 5 */
+} superblock;
 
 typedef struct block {
-uint8_t block[1024];
-}block;
+  uint8_t block[1024];
+} block;
